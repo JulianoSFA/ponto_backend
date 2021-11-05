9ab74206f34af7aa9f76217e5e1603f54bcfb121
Juliano Sartorelli <juliano.franco@datacake.com.br>
2021-11-04 22:00:00 -0300

    Adicionando o usuario no timeblock, que não existia quando o modelo foi criado

--- apps/ponto/models/TimeBlock.py
+++ apps/ponto/models/TimeBlock.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+from apps.core.users.models import User
+
+
 class TimeBlock(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     day = models.DateField(null=True, blank=True)
     next_time_block = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True, related_name='last_time')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True, related_name='last_time')
 
     class Meta:
         abstract = True
